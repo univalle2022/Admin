f959fdf7a1792e4d5dcb431813aec81e2607d6e6
Yorchis <jorgeluisf180@example.com>
2022-11-21 00:30:12 -0400

    sidebar contratos y descuentos

--- Config/Config.php
+++ Config/Config.php
@@ -1,14 +1,14 @@
 <?php 
 
-    const BASE_URL = "http://localhost/Sistema_new";
+    const BASE_URL = "http://localhost/Ecommerce";
 
     //Zona horaria
 	date_default_timezone_set('UTC');
     
     const DB_HOST = "localhost";
-    const DB_NAME = "bdromjuli";
+    const DB_NAME = "ecommerce";
     const DB_USER = "root";
-    const DB_PASSWORD = "";
+    const DB_PASSWORD = "root";
     const DB_CHARSET = "utf8";
 
     const NOMBRE_REMITENTE = "Tienda de Ropa Romeo y Julieta";
